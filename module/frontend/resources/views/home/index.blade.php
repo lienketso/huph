45277809b2df002ff045f6dc75f718f6715f4a1f
Tên nlienketso <thanhan1507@gmail.com>
2024-05-19 12:52:14 +0700

    update css title news

--- module/frontend/resources/views/home/index.blade.php
+++ module/frontend/resources/views/home/index.blade.php
@@ -267,7 +267,7 @@
     <div class="container">
         <div class="row">
             <div class="col text-center">
-                <h2>{!! $setting['keyword_8_'.$lang] !!}</h2>
+                <h2 class="title-news-home">{!! $setting['keyword_8_'.$lang] !!}</h2>
             </div>
         </div>
         <div class="row">
