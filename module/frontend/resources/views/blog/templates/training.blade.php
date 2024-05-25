29a0a2bcc8a7ff76c67892fa832f5422d1e6d9fd
lienketso <thanhan1507@gmail.com>
2024-05-25 11:07:25 +0700

    update post not child

--- module/frontend/resources/views/blog/templates/training.blade.php
+++ module/frontend/resources/views/blog/templates/training.blade.php
@@ -64,25 +64,39 @@
                     </ul>
                     <div class="tab-content tab-content-nganh" id="">
                         @if($child->childs()->exists())
+                            @php
+                                $postsNganh = $child->posts()->where('status','active')->limit(16)->get();
+                            @endphp
                             @foreach($child->childs as $key=>$c)
                         <div class="tab-pane fade {{($key==0)?'show active':''}}" id="home-tab-pane{{$c->id}}" role="tabpanel" aria-labelledby="home-tab{{$c->id}}"
                              tabindex="{{$key}}">
-                            @if($c->childs()->exists())
+
                             <div class="list-post-nganh">
                                 <div class="row">
-                                    @foreach($c->childs as $three)
-                                    <div class="col-lg-3">
-                                        <a class="item-post-nganh"
-                                           href="{{route('frontend::blog.index.get',$three->slug)}}"
-                                           style="background-image: url('{{($three->thumbnail!='') ? upload_url($three->thumbnail) : asset('admin/themes/images/no-image.png')}}')">
-                                            <span>{{$three->name}}</span>
-                                        </a>
-                                    </div>
-                                    @endforeach
-
+                                    @if($c->childs()->exists())
+                                        @foreach($c->childs as $three)
+                                        <div class="col-lg-3">
+                                            <a class="item-post-nganh"
+                                               href="{{route('frontend::blog.index.get',$three->slug)}}"
+                                               style="background-image: url('{{($three->thumbnail!='') ? upload_url($three->thumbnail) : asset('admin/themes/images/no-image.png')}}')">
+                                                <span>{{$three->name}}</span>
+                                            </a>
+                                        </div>
+                                        @endforeach
+                                        @else
+                                        @foreach($postsNganh as $p)
+                                            <div class="col-lg-3">
+                                                <a class="item-post-nganh"
+                                                   href="{{route('frontend::blog.detail.get',$p->slug)}}"
+                                                   style="background-image: url('{{($p->thumbnail!='') ? upload_url($p->thumbnail) : asset('admin/themes/images/no-image.png')}}')">
+                                                    <span>{{cut_string($p->name,35)}}</span>
+                                                </a>
+                                            </div>
+                                        @endforeach
+                                    @endif
                                 </div>
                             </div>
-                                @endif
+
                         </div>
                             @endforeach
                         @endif
@@ -95,7 +109,7 @@
         </div>
     </section>
     @endif
-    
+
     @include('frontend::form')
     @include('frontend::latest')
 @endsection
