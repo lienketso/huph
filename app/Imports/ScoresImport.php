63a6ce5284eaa922b8953bc4e0979e72a6037d50
lienketso <thanhan1507@gmail.com>
2024-09-10 10:38:26 +0700

    import điểm thi check nếu đã có số cccd

--- app/Imports/ScoresImport.php
+++ app/Imports/ScoresImport.php
@@ -26,24 +26,49 @@ class ScoresImport implements ToModel
             return null;
         }
 
-        return new Scores([
-            'identification_number'=> (!empty($row[0]) ? $row[0] : "0"), // số báo danh
-            'cccd_number' => (!empty($row[1]) ? $row[1] : "-"),
-            'name'     => (!empty($row[2]) ? $row[2] : "-"), // Họ và tên
-            'gender'     => (!empty($row[3]) ? $row[3] : "-"), // Giới tính
-            'birthday' => (!empty($row[4]) ? $row[4] : "-"), // Ngày sinh
-            'test_subject' => (!empty($row[5]) ? $row[5] : "-"), //Đối tượng dự thi
-            'score_one_name' => (!empty($row[6]) ? $row[6] : ""), // Tên môn thi 1
-            'score_one' => (!empty($row[7]) ? $row[7] : ""), // Điểm thi môn 1
-            'priority_score_one' => (!empty($row[8]) ? $row[8] : "0"), // Điểm ưu tiên môn 1
-            'total_score_one' => (!empty($row[9]) ? $row[9] : ""), // Tổng điểm môn thi 1
-            'score_two_name' => (!empty($row[10]) ? $row[10] : ""), // Tên môn thi 2
-            'score_two' => (!empty($row[11]) ? $row[11] : ""), //Điểm môn thi 2
-            'priority_score_two' => (!empty($row[12]) ? $row[12] : "0"), //Điểm ưu tiên môn 2
-            'total_score_two' => (!empty($row[13]) ? $row[13] : ""), // Tổng điểm môn 2
-            'total_scores' => (!empty($row[14]) ? $row[14] : ""), //Tổng điểm xét tuyển
-            'comment' => (!empty($row[15]) ? $row[15] : "-"), //Ghi chú
-            'status' => (!empty($row[16]) ? $row[16] : "pending"), //Trạng thái
-        ]);
+        // Kiểm tra nếu cccd_number đã tồn tại
+        $existingScore = Scores::where('cccd_number', $row[1])->first();
+        if ($existingScore) {
+            $existingScore->update([
+                'identification_number'=> (!empty($row[0]) ? $row[0] : "0"), // số báo danh
+                'name'     => (!empty($row[2]) ? $row[2] : "-"), // Họ và tên
+                'gender'     => (!empty($row[3]) ? $row[3] : "-"), // Giới tính
+                'birthday' => (!empty($row[4]) ? $row[4] : "-"), // Ngày sinh
+                'test_subject' => (!empty($row[5]) ? $row[5] : "-"), //Đối tượng dự thi
+                'score_one_name' => (!empty($row[6]) ? $row[6] : ""), // Tên môn thi 1
+                'score_one' => (!empty($row[7]) ? $row[7] : ""), // Điểm thi môn 1
+                'priority_score_one' => (!empty($row[8]) ? $row[8] : "0"), // Điểm ưu tiên môn 1
+                'total_score_one' => (!empty($row[9]) ? $row[9] : ""), // Tổng điểm môn thi 1
+                'score_two_name' => (!empty($row[10]) ? $row[10] : ""), // Tên môn thi 2
+                'score_two' => (!empty($row[11]) ? $row[11] : ""), //Điểm môn thi 2
+                'priority_score_two' => (!empty($row[12]) ? $row[12] : "0"), //Điểm ưu tiên môn 2
+                'total_score_two' => (!empty($row[13]) ? $row[13] : ""), // Tổng điểm môn 2
+                'total_scores' => (!empty($row[14]) ? $row[14] : ""), //Tổng điểm xét tuyển
+                'comment' => (!empty($row[15]) ? $row[15] : "-"), //Ghi chú
+                'status' => (!empty($row[16]) ? $row[16] : "pending"), //Trạng thái
+            ]);
+            return $existingScore; // Trả về bản ghi đã cập nhật
+        }else{
+            return new Scores([
+                'identification_number'=> (!empty($row[0]) ? $row[0] : "0"), // số báo danh
+                'cccd_number' => (!empty($row[1]) ? $row[1] : "-"),
+                'name'     => (!empty($row[2]) ? $row[2] : "-"), // Họ và tên
+                'gender'     => (!empty($row[3]) ? $row[3] : "-"), // Giới tính
+                'birthday' => (!empty($row[4]) ? $row[4] : "-"), // Ngày sinh
+                'test_subject' => (!empty($row[5]) ? $row[5] : "-"), //Đối tượng dự thi
+                'score_one_name' => (!empty($row[6]) ? $row[6] : ""), // Tên môn thi 1
+                'score_one' => (!empty($row[7]) ? $row[7] : ""), // Điểm thi môn 1
+                'priority_score_one' => (!empty($row[8]) ? $row[8] : "0"), // Điểm ưu tiên môn 1
+                'total_score_one' => (!empty($row[9]) ? $row[9] : ""), // Tổng điểm môn thi 1
+                'score_two_name' => (!empty($row[10]) ? $row[10] : ""), // Tên môn thi 2
+                'score_two' => (!empty($row[11]) ? $row[11] : ""), //Điểm môn thi 2
+                'priority_score_two' => (!empty($row[12]) ? $row[12] : "0"), //Điểm ưu tiên môn 2
+                'total_score_two' => (!empty($row[13]) ? $row[13] : ""), // Tổng điểm môn 2
+                'total_scores' => (!empty($row[14]) ? $row[14] : ""), //Tổng điểm xét tuyển
+                'comment' => (!empty($row[15]) ? $row[15] : "-"), //Ghi chú
+                'status' => (!empty($row[16]) ? $row[16] : "pending"), //Trạng thái
+            ]);
+        }
+
     }
 }
